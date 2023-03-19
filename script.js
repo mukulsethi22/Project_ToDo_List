a80e086166eb08c3862d18c87f3aa0a0a1aec668
Mukul Sethi <mukulsethi2001@gmail.com>
2023-03-19 13:55:08 +0530

    dynamic buttons

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let ulTasks = $('#ulTasks')
 let btnAdd = $('#btnAdd')
 let btnReset = $('#btnReset')
 let btnCleanup = $('#btnCleanup')
+let btnSort = $('#btnSort')
 let inpNewTask = $('#inpNewTask')
 
 
@@ -16,16 +17,35 @@ function addItem(){
     })
     ulTasks.append(listItem)
     inpNewTask.val('')
+    toggleInputButtons()
 }
 
 function clearDone(){
  $('#ulTasks .done').remove()
+ toggleInputButtons()
+}
+
+function sortTasks(){
+    $('#ulTasks .done').appendTo(ulTasks)
+}
+
+function toggleInputButtons(){
+    btnReset.prop('disabled',inpNewTask.val()=='')
+    btnAdd.prop('disabled',inpNewTask.val()=='')
+    btnSort.prop('disabled',ulTasks.children().length < 1)
+    btnCleanup.prop('disabled',ulTasks.children().length<1)
 }
 
 inpNewTask.keypress((e)=>{
     if(e.which == 13)addItem()
 })
 
+inpNewTask.on('input',toggleInputButtons)
+
 btnAdd.click(addItem)
-btnReset.click(()=>inpNewTask.val(''))
+btnReset.click(()=>{
+    inpNewTask.val('')
+    toggleInputButtons()
+})
 btnCleanup.click(clearDone)
+btnSort.click(sortTasks)
